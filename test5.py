ede73239df98bbc659e8af1a3083777f29dffef8
YasinDav <githubyasin@gmail.com>
2024-06-09 21:19:49 +0330

    recorrect between func

--- test5.py
+++ test5.py
@@ -2,7 +2,65 @@ from html.parser import HTMLParser
 import requests
 from bs4 import BeautifulSoup
 
+html_tags_list = [
+    ('html', 'html'), ('head', 'head'), ('title', 'title'), ('base',),
+    ('link',), ('meta',), ('style', 'style'), ('script', 'script'),
+    ('noscript', 'noscript'), ('body', 'body'), ('section', 'section'),
+    ('nav', 'nav'), ('article', 'article'), ('aside', 'aside'), ('h1', 'h1'),
+    ('h2', 'h2'), ('h3', 'h3'), ('h4', 'h4'), ('h5', 'h5'), ('h6', 'h6'),
+    ('header', 'header'), ('footer', 'footer'), ('address', 'address'),
+    ('p', 'p'), ('hr',), ('pre', 'pre'), ('blockquote', 'blockquote'), ('ol', 'ol'),
+    ('ul', 'ul'), ('li', 'li'), ('dl', 'dl'), ('dt', 'dt'), ('dd', 'dd'),
+    ('figure', 'figure'), ('figcaption', 'figcaption'), ('main', 'main'), ('div', 'div'),
+    ('a', 'a'), ('em', 'em'), ('strong', 'strong'), ('small', 'small'), ('s', 's'),
+    ('cite', 'cite'), ('q', 'q'), ('dfn', 'dfn'), ('abbr', 'abbr'), ('ruby', 'ruby'),
+    ('rt', 'rt'), ('rp', 'rp'), ('data', 'data'), ('time', 'time'), ('code', 'code'),
+    ('var', 'var'), ('samp', 'samp'), ('kbd', 'kbd'), ('sub', 'sub'), ('sup', 'sup'),
+    ('i', 'i'), ('b', 'b'), ('u', 'u'), ('mark', 'mark'), ('bdi', 'bdi'), ('bdo', 'bdo'),
+    ('span', 'span'), ('br',), ('wbr',), ('ins', 'ins'), ('del', 'del'),
+    ('picture', 'picture'), ('source',), ('img',), ('iframe', 'iframe'), ('embed',),
+    ('object', 'object'), ('param',), ('video', 'video'), ('audio', 'audio'), ('track',),
+    ('map', 'map'), ('area',), ('table', 'table'), ('caption', 'caption'),
+    ('colgroup', 'colgroup'), ('col',), ('tbody', 'tbody'), ('thead', 'thead'),
+    ('tfoot', 'tfoot'), ('tr', 'tr'), ('td', 'td'), ('th', 'th'), ('form', 'form'),
+    ('label', 'label'), ('input',), ('button', 'button'), ('select', 'select'),
+    ('datalist', 'datalist'), ('optgroup', 'optgroup'), ('option', 'option'),
+    ('textarea', 'textarea'), ('output', 'output'), ('progress', 'progress'),
+    ('meter', 'meter'), ('fieldset', 'fieldset'), ('legend', 'legend'),
+    ('details', 'details'), ('summary', 'summary'), ('dialog', 'dialog'),
+    ('script', 'script'), ('noscript', 'noscript'), ('template', 'template'),
+    ('canvas', 'canvas')
+]
+# List of common HTML tags
+html_tags = [
+    "html", "head", "title", "base", "link", "meta", "style", "script", "noscript", "body",
+    "section", "nav", "article", "aside", "h1", "h2", "h3", "h4", "h5", "h6", "header",
+    "footer", "address", "p", "hr", "pre", "blockquote", "ol", "ul", "li", "dl", "dt", "dd",
+    "figure", "figcaption", "main", "div", "a", "em", "strong", "small", "s", "cite", "q",
+    "dfn", "abbr", "ruby", "rt", "rp", "data", "time", "code", "var", "samp", "kbd", "sub",
+    "sup", "i", "b", "u", "mark", "bdi", "bdo", "span", "br", "wbr", "ins", "del", "picture",
+    "source", "img", "iframe", "embed", "object", "param", "video", "audio", "track", "map",
+    "area", "table", "caption", "colgroup", "col", "tbody", "thead", "tfoot", "tr", "td",
+    "th", "form", "label", "input", "button", "select", "datalist", "optgroup", "option",
+    "textarea", "output", "progress", "meter", "fieldset", "legend", "details", "summary",
+    "dialog", "script", "noscript", "template", "canvas"
+]
+# Tags that do not have closing tags
+self_closing_tags = ["area", "base", "br", "col", "embed", "hr", "img", "input", "link", "meta", "param", "source", "track", "wbr"]
+
+
 class Html:
+    closed_tag_html = [
+        ("!", "--"), ("!DOCTYPE html"), ("a", "a"), ("abbr", "abbr"),
+        ("address", "address"), ("area", "area"), ("map", "map"),
+        ("article", "article"), ("aside", "aside"), ("b", "b"), ("base"),
+        ("bdo", "bdo"), ("blockquote", "blockquote"), ("body", "body"),
+        ("br"), ("button", "button"), ("<caption>", "caption"), ("cite", "cite"),
+        ("code", "code"), ("col"), ("colgroup", "colgroup"), ("command", "command"),
+        ("details", "details"), ("div", "div"), ("datalist", "datalist"), ("dd", "dd"),
+        ("del", "del"), ("dfn", "dfn"), ("dialog", "dialog"), ("dl", "dl"), ("dt", "dt"),
+        ("data", "data"), ("em", "em"),
+    ]
 
     def __init__(self, html:str):
         self.html = html
@@ -81,22 +139,24 @@ class Html:
         return list_dict
 
     def between(self, tag):
-        between_list = []
-        tags=self.list_tag()
-        print(tags)
-        index_list = []
-        for i in tags:
-            if i[0] == tag:
-                index_list.append(tags.index(i))
-            if i[0] == "/"+tag:
-                index_list.append(tags.index(i))
-        print(index_list)
-        for i in range(0, len(index_list), 2):
-             b_list = []
-             for j in range(index_list[i], index_list[i+1]):
-                 b_list.append(self.get_tag_map(tags[j]))
-             between_list.append(b_list)
-        return between_list
+        global self_closing_tags
+        html = self.html
+        if tag in self_closing_tags:
+            return ["0"]
+        else:
+            if html.find(tag) == -1:
+                return ["1"]
+            else:
+                list4 = []
+                s=0
+
+                lenght = html.count(tag)/2
+                for i in range(int(lenght)):
+                    start=html.find(tag, s+len(tag), len(html))
+                    s=start
+                    end=html.rfind("/"+tag, s+len(tag), len(html))
+                    list4.append(html[start-1:end+len(tag)+2].replace("\n", "").replace("    ", ""))
+        return list4
 
     def remove_comment(self):
         tags = self.html.split("\n")
@@ -245,7 +305,7 @@ omid_html = """
    </body>
 </html>
 """
-
+h=Html(omid_html)
 
 #
 # h=Html(omid_html)
@@ -277,66 +337,81 @@ omid_html = """
 # line("#####")
 # h.remove_space()
 # print(h)
-again = True
-url = input(f"{'\x1b[38;5;4m'}Enter url : ")
-html_code_url = ""
-try:
-    page = requests.get(url)
-    sup = BeautifulSoup(page.content, "html.parser")
-    html_code_url = sup.prettify()
-    print(f"{'\033[32m'}Connected to "+f"{'\033[00m'}"+url.split(".")[1])
-    h = Html(html_code_url)
-    line("\033[93m" + "Html code")
-    print('\033[00m' + html_code_url)
-    line("\033[93m" + "Command")
-    again_code = ""
-except:
-    print(f"{"\033[31m"}Something went")
-    again = False
-while again:
-    x = input(f"{'\033[32m'}Choice one [tag, list_tag, head, body, find_tag, get_tag_map, between, remove_comment, remove_space, clean_html, exit, a (again)] : ")
-    if x == "a":
-        if again_code == "":
-            line("\033[93m" + "Don't enter anything")
-        x = again_code
-    if x == "exit":
-        line("\033[93m"+"Exit")
-        again = False
-        break
-    elif x == "tag":
-        line("\033[93m"+"tag")
-        print('\033[00m'+str(h.tag()))
-    elif x == "list_tag":
-        line("\033[93m"+"list tag")
-        print('\033[00m'+str(h.list_tag()))
-    elif x == "head":
-        line(f"{"\033[93m"}header")
-        print('\033[00m'+str(h.head()))
-    elif x == "body":
-        line(f"{"\033[93m"}body")
-        print(f"{"\033[35m"}body")
-        print('\033[00m'+str(h.body()))
-    elif x == "find_tag":
-        line(f"{"\033[93m"}find tag")
-        print('\033[00m'+str(h.finde_tag(input(f"{'\033[32m'}Enter tag name : "))))
-    elif x == "get_tag_map":
-        line(f"{"\033[93m"}get tag map")
-        print('\033[00m'+str(h.get_tag_map(input(f"{'\033[32m'}Enter tag name : "))))
-    elif x == "between":
-        line(f"{"\033[93m"}between")
-        print('\033[00m'+str(h.between(input(f"{'\033[32m'}Enter between tag name : "))))
-    elif x== "remove_comment":
-        line(f"{"\033[93m"}remove comment")
-        h.remove_comment()
-        print('\033[00m'+str(h))
-    elif x == "remove_space":
-        line(f"{"\033[93m"}remove space")
-        h.remove_space()
-        print('\033[00m'+str(h))
-    elif x == "clean_html":
-        line(f"{"\033[93m"}clean html")
-        h.clean_html()
-        print('\033[00m'+str(h))
-    else:
-        line(f"{"\033[31m"}unknown command")
-    again_code = x
+
+
+# again = True
+# url = input(f"{'\x1b[38;5;4m'}Enter url : ")
+# if url == "":
+#     html_code_url = omid_html
+#     h = Html(html_code_url)
+#     print(f"{'\033[32m'}Connected to local html code")
+#     line("\033[93m" + "Html code")
+#     print('\033[00m' + html_code_url)
+#     line("\033[93m" + "Command")
+#     again_code = ""
+# else:
+#     try:
+#         page = requests.get(url)
+#         sup = BeautifulSoup(page.content, "html.parser")
+#         html_code_url = sup.prettify()
+#         print(f"{'\033[32m'}Connected to "+f"{'\033[00m'}"+url.split(".")[1])
+#         h = Html(html_code_url)
+#         line("\033[93m" + "Html code")
+#         print('\033[00m' + html_code_url)
+#         line("\033[93m" + "Command")
+#         again_code = ""
+#     except:
+#         print(f"{"\033[31m"}Something went")
+#         again = False
+# while again:
+#     x = input(f"{'\033[32m'}Choice one [tag, list_tag, head, body, find_tag, get_tag_map, between, remove_comment, remove_space, clean_html, exit, a (again)] : ")
+#     if x == "a":
+#         if again_code == "":
+#             line("\033[93m" + "Don't enter anything")
+#         else:
+#             x = again_code
+#     if x == "exit":
+#         line("\033[93m"+"Exit")
+#         again = False
+#         break
+#     elif x == "tag":
+#         line("\033[93m"+"tag")
+#         print('\033[00m'+str(h.tag()))
+#     elif x == "list_tag":
+#         line("\033[93m"+"list tag")
+#         print('\033[00m'+str(h.list_tag()))
+#     elif x == "head":
+#         line(f"{"\033[93m"}header")
+#         print('\033[00m'+str(h.head()))
+#     elif x == "body":
+#         line(f"{"\033[93m"}body")
+#         print(f"{"\033[35m"}body")
+#         print('\033[00m'+str(h.body()))
+#     elif x == "find_tag":
+#         line(f"{"\033[93m"}find tag")
+#         print('\033[00m'+str(h.finde_tag(input(f"{'\033[32m'}Enter tag name : "))))
+#     elif x == "get_tag_map":
+#         line(f"{"\033[93m"}get tag map")
+#         print('\033[00m'+str(h.get_tag_map(input(f"{'\033[32m'}Enter tag name : "))))
+#     elif x == "between":
+#         line(f"{"\033[93m"}between tags")
+#         print('\033[00m'+str(h.between(input(f"{'\033[32m'}Enter between tag name : "))))
+#     elif x== "remove_comment":
+#         line(f"{"\033[93m"}remove comment")
+#         h.remove_comment()
+#         print('\033[00m'+str(h))
+#     elif x == "remove_space":
+#         line(f"{"\033[93m"}remove space")
+#         h.remove_space()
+#         print('\033[00m'+str(h))
+#     elif x == "clean_html":
+#         line(f"{"\033[93m"}clean html")
+#         h.clean_html()
+#         print('\033[00m'+str(h))
+#     else:
+#         line(f"{"\033[31m"}unknown command")
+#     again_code = x
+
+m=h.between("div")
+for i in m:
+    print(i+"\n")
